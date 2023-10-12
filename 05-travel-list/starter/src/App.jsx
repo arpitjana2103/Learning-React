5d261a9c475a86ca1361d06e15f0e018e9123b93
Arpit Jana <arpitjana2103@gmail.com>
2023-10-12 17:19:06 +0530

    Static Design Updated

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -1,3 +1,10 @@
+const initialItems = [
+    {id: 1, description: 'Passports', quantity: 2, packed: false},
+    {id: 2, description: 'Socks', quantity: 12, packed: false},
+    {id: 3, description: 'Charger', quantity: 1, packed: true},
+    {id: 4, description: 'Glass', quantity: 2, packed: true},
+];
+
 function App() {
     return (
         <div className="app">
@@ -30,7 +37,27 @@ function Form() {
 }
 
 function PackingList() {
-    return <div className="list">List</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({item}) {
+    const style = {textDecoration: 'line-through'};
+    return (
+        <li>
+            <span style={item.packed ? style : null}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
 }
 
 function Stats() {
