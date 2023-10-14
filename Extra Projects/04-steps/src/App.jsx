83ae4ec36d4f1c9522d88aebbf1f76a131e484e1
Arpit Jana <arpitjana2103@gmail.com>
2023-10-14 10:55:05 +0530

    Start Using Children Prop

---
+++ Extra Projects/04-steps/src/App.jsx
@@ -0,0 +1,71 @@
+import {useState} from 'react';
+
+const message = [
+    'Learn React ⚛',
+    'Apply for jobs 👜',
+    'Invest you new income 🤑',
+];
+
+export default function App() {
+    const [step, setStep] = useState(1);
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handlePrevious() {
+        if (step === 1) return;
+        setStep((s) => s - 1);
+    }
+
+    function handleNext() {
+        if (step === message.length) return;
+        setStep((s) => s + 1);
+    }
+
+    return (
+        <>
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
+                &times;
+            </button>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+                    </div>
+                    <p className="message">Hello</p>
+                    <p className="message">
+                        Step {step}: {message[step - 1]}
+                    </p>
+                    <div className="buttons">
+                        <Button
+                            textColor="#fff"
+                            bgColor="#7850f2"
+                            onClick={handlePrevious}
+                        >
+                            <span>👈</span>Previous
+                        </Button>
+                        <Button
+                            textColor="#fff"
+                            bgColor="#7850f2"
+                            onClick={handleNext}
+                        >
+                            Next<span>👉</span>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+function Button({textColor, bgColor, onClick, children}) {
+    return (
+        <button
+            style={{backgroundColor: bgColor, color: textColor}}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
