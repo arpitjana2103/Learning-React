333b17e7591dbf68cf73cb05494281bc261e6dd1
Arpit Jana <arpitjana2103@gmail.com>
2023-11-01 15:01:09 +0530

    Citi List Implemented

--- Extra Projects/10-worldwise/src/components/CityList.jsx
+++ Extra Projects/10-worldwise/src/components/CityList.jsx
@@ -1,6 +1,20 @@
 import styles from './CityList.module.css';
-function CityList() {
-    return <ul className={styles.cityList}>LIST LIST</ul>;
+import Spinner from '../components/Spinner';
+import CityItem from './CityItem';
+import Message from '../components/Message';
+
+function CityList({cities, isLoading}) {
+    if (isLoading) return <Spinner />;
+    if (cities.length === 0) {
+        return <Message message="Add your first city by clicking on the Map" />;
+    }
+    return (
+        <ul className={styles.cityList}>
+            {cities.map(function (city) {
+                return <CityItem key={city.id} city={city} />;
+            })}
+        </ul>
+    );
 }
 
 export default CityList;
