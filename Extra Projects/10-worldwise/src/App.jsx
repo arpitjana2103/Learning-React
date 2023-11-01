fd2be2a87056f53f5202d2808e73b00a5d0681d9
Arpit Jana <arpitjana2103@gmail.com>
2023-11-01 08:55:06 +0530

    Added Starter Files

--- Extra Projects/10-worldwise/src/App.jsx
+++ Extra Projects/10-worldwise/src/App.jsx
@@ -3,6 +3,8 @@ import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import './index.css';
 
 function App() {
     return (
@@ -13,6 +15,7 @@ function App() {
                     <Route path="/" element={<Homepage />} />
                     <Route path="product" element={<Product />} />
                     <Route path="pricing" element={<Pricing />} />
+                    <Route path="app" element={<AppLayout />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
--- Extra Projects/10-worldwise/src/pages/Pricing.jsx
+++ Extra Projects/10-worldwise/src/pages/Pricing.jsx
@@ -1,11 +1,24 @@
-import PageNav from '../components/pageNav';
-function Pricing() {
-    return (
-        <div>
-            <PageNav />
-            <h3>Pricing</h3>
-        </div>
-    );
-}
+// Uses the same styles as Product
+import styles from "./Product.module.css";
 
-export default Pricing;
+export default function Product() {
+  return (
+    <main className={styles.product}>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
+  );
+}
