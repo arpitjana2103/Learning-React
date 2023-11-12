808d98fa0b50e024a0701a831163451e6908357c
Arpit Jana <arpitjana2103@gmail.com>
2023-11-12 18:40:45 +0530

    Redux Intro Completed

--- Extra Projects/16-redux-intro/src/redux-store/store.js
+++ Extra Projects/16-redux-intro/src/redux-store/store.js
@@ -1,6 +1,6 @@
 import {combineReducers, createStore} from 'redux';
-import accountReducer from './features/accounts/accountSlice';
-import customerReducer from './features/customers/customerSlice';
+import accountReducer from './accountSlice';
+import customerReducer from './customerSlice';
 
 const rootReducer = combineReducers({
     account: accountReducer,
