7b4ec69b5214d2787bc6d757ac2efb99f0cd38d4
Arpit Jana <arpitjana2103@gmail.com>
2023-11-13 19:01:16 +0530

    createSlice integrated in CustomerSlice

--- Extra Projects/16-redux-intro/src/redux-store/customerSlice.js
+++ Extra Projects/16-redux-intro/src/redux-store/customerSlice.js
@@ -1,5 +1,35 @@
-const initialStateCustomer = {
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
     fullName: '',
     nationaleID: '',
     createdAt: '',
 };
+
+const customerSlice = createSlice({
+    name: 'customer',
+    initialState: initialState,
+
+    reducers: {
+        createCustomer: {
+            prepare: function (fullName, nationaleID) {
+                return {
+                    payload: {fullName, nationaleID},
+                };
+            },
+
+            reducer: function (state, action) {
+                state.fullName = action.payload.fullName;
+                state.nationaleID = action.payload.nationaleID;
+                state.createdAt = new Date().toISOString();
+            },
+        },
+
+        updateName: function (state, action) {
+            state.fullName = action.payload;
+        },
+    },
+});
+
+export const {createCustomer, updateName} = customerSlice.actions;
+export default customerSlice.reducer;
